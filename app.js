07457ef8b23369a982f5addee328cd3545f4d098
rafidzia <32030497+rafidzia@users.noreply.github.com>
2020-09-07 01:22:56 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const days = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jum'at", "Sabtu"];
 const get = require("./get");
 (async () => {
-    var schedule = await get("https://ethol.pens.ac.id/api/v1/schedules?user=17210&year=2019&semester=2&role=2")
-    var room = await get("https://ethol.pens.ac.id/api/v1/rooms?kuliah=[41656,41657,41658,41659,41660,41661,41662,41663,41664,41665,41666]")
+    // var schedule = await get("https://ethol.pens.ac.id/api/v1/schedules?user=17210&year=2019&semester=2&role=2")
+    var schedule = await get("https://ethol.pens.ac.id/api/v1/schedules?user=17210&year=2020&semester=1&role=2")
+    // var room = await get("https://ethol.pens.ac.id/api/v1/rooms?kuliah=[41656,41657,41658,41659,41660,41661,41662,41663,41664,41665,41666]")
+    var room = await get("https://ethol.pens.ac.id/api/v1/rooms?kuliah=[42974,42975,42976,42977,42978,42979,42980,42981,42982,42983,42984,42985,42986]")
     var savedSchedule = schedule
     var lastDay = ""
     var check = ()=>{
